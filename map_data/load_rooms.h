335b6bf35062a4e555ad696d0f0ef8f9c8f87b34
scrmbld <liamallen792@gmail.com>
2020-02-16 13:15:27 -0800

    created layout randomization

--- map_data/load_rooms.h
+++ map_data/load_rooms.h
@@ -26,18 +26,3 @@ void load_rooms(vector<vector<vector<char>>> &room_data, const vector<string> &p
 
 }
 
-int main() {
-	vector<vector<vector<char>>> room_data;
-	vector<string> paths = {"room_01.txt", "room_02.txt"};
-	load_rooms(room_data, paths);
-
-	for (int i = 0; i < room_data.size(); i++) {
-		cout << paths.at(i) << endl;
-		for (const auto &v : room_data.at(i)) {
-			for (char c : v) {
-				cout << c;
-			}
-			cout << endl;
-		}
-	}
-}
--- map_data/map.h
+++ map_data/map.h
@@ -6,10 +6,15 @@
 #include <fstream>
 #include <random>
 #include <ncurses.h>
-using namespace std; //Boo hiss
+#include "load_rooms.h"
+using namespace std; 
 
 class Map {
-	vector<vector<char>> map;
+	vector<vector<char>> view;//the stuff that actually gets printed to the screen
+	vector<vector<vector<char>>> rooms;//stores all the rooms
+	vector<vector<int>> layout;//stores how the rooms are arranged
+	//file paths for rooms
+	const vector<string> paths = {"room_01.txt", "room_02.txt"};
 	default_random_engine gen;
 	public:
 	static const char HERO     = 'H';
@@ -18,24 +23,42 @@ class Map {
 	static const char WATER    = '~';
 	static const char OPEN     = '.';
 	static const char TREASURE = '$';
-	static const size_t WIDTH = 36; //World is a 100x100 map
-	static const size_t HEIGHT = 20;
-	static const size_t DISPLAY = 19; //Show a 10x10 area at a time
-	//Randomly generate map
+	static const size_t WIDTH = 216; 
+	static const size_t HEIGHT = 120;
+	static const size_t DISPLAY = 19;
+	static const size_t LAYOUT_SIZE = 6;
+	static const size_t ROOM_WIDTH = 36;
+	static const size_t ROOM_HEIGHT = 20;
+	//load map
 	void init_map() {
-		string filename = "room_01.txt";
-		ifstream ins(filename);
-		while (ins) {
-			string s;
-			getline(ins, s);
-			if (!ins) break;
-			
-			vector<char> vec;
-			for (char c : s) { vec.push_back(c); }
-			map.push_back(vec);
+		uniform_int_distribution<int> d100(1, 100);
+		layout.clear();
+		for (int i = 0; i < LAYOUT_SIZE; i++) {
+			vector<int> row;
+			for (int j = 0; j < LAYOUT_SIZE; j++) {
+				row.push_back(d100(gen) % paths.size());
+			}
+			layout.push_back(row);
 		}
-		if (map.size() != HEIGHT && map.at(0).size() != WIDTH) cout << "error reading map\n";
+
+		//iterates through the different roooms on the y axis
+		for (int i = 0; i < LAYOUT_SIZE; i++) {
+			//iterates through contents of room on y axis
+			for (int j = 0; j < ROOM_HEIGHT; j++) {
+				//iterates through the different rooms on the x axis
+				vector<char> row;
+				for (int k = 0; k < LAYOUT_SIZE; k++) {
+					//iterates through contents of room on x axis
+					for (int l = 0; l < ROOM_WIDTH; l++) {
+						row.push_back(rooms.at(layout.at(i).at(k)).at(j).at(l));
+					}
+				}
+				view.push_back(row);
+			}
+		}
+
 	}
+
 	//Draw the DISPLAY tiles around coordinate (x,y)
 	void draw(int x, int y) {
 		int start_x = x - DISPLAY/2;
@@ -68,13 +91,13 @@ class Map {
 				//if (i == cursor_x && j == cursor_y)
 				//	attron(A_UNDERLINE | A_BOLD);
 				int color = 1;
-				if (map.at(i).at(j) == WALL)
+				if (view.at(i).at(j) == WALL)
 					color = 5;
-				else if (map.at(i).at(j) == WATER)
+				else if (view.at(i).at(j) == WATER)
 					color = 2;
-				else if (map.at(i).at(j) == TREASURE)
+				else if (view.at(i).at(j) == TREASURE)
 					color = 4;
-				else if (map.at(i).at(j) == MONSTER)
+				else if (view.at(i).at(j) == MONSTER)
 					color = 6;
 				if (i == y && j == x) {
 					color = 3;
@@ -85,7 +108,7 @@ class Map {
 					mvaddch(i-start_y,j-start_x, HERO);
 				} else {
 					attron(COLOR_PAIR(color));
-					mvaddch(i-start_y,j-start_x,map.at(i).at(j));
+					mvaddch(i-start_y,j-start_x,view.at(i).at(j));
 				}
 				attroff(COLOR_PAIR(color));
 				//attroff(A_UNDERLINE | A_BOLD);
@@ -94,12 +117,13 @@ class Map {
 
 	}
 	Map() {
+		load_rooms(rooms, paths);
 		init_map();
 	}
 
 	//returns whatever character is in the map at the provided coordinates
 	char spot_data(int x, int y) {
 		assert(x < WIDTH && y < HEIGHT && x >= 0 && y >= 0);
-		return map.at(y).at(x);
+		return view.at(y).at(x);
 	}
 };

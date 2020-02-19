cff644658c318973242fdc8a2a988c725a19acfd
scrmbld <liamallen792@gmail.com>
2020-02-19 12:30:49 -0800

    switched test combat to linked list

--- combat_data/combat_test.cc
+++ combat_data/combat_test.cc
@@ -44,40 +44,44 @@ void combat_mode() {
 		new CircSLelement<Fighter>(Fighter("Haigen - The Troll", 100, 10, 2, 1), "Enemy"),
 		new CircSLelement<Fighter>(CircSLelement<Fighter>(Fighter("The Wizard", 10, 2, 0.5, 1), "Player"))
 	};
+	encounter[0]->setNext(encounter[1]);
+	encounter[1]->setNext(encounter[0]);
 	
-	cout << "\n ✳ " << encounter->getNext() << " goes first.\n";
-	
-	while (active->get_health() > 0){	
-        cout << "\n (╯°□°)╯︵◓" << endl;
-        cout << "\n " << active->get_name() << " is attacking " << inactive->get_name() << endl;
+	CircSLelement<Fighter> *temp = encounter[0];
+	/*
+	while (true) {
+		cout << "\n ✳ " << temp->getValue() << " goes first.\n";
+		temp = temp->getNext();
+	}
+	*/
+	CircSLelement<Fighter> *active = encounter[0];
+	CircSLelement<Fighter> *inactive = active->getNext();
+	while (active->getValue().get_health() > 0){	
+        inactive = active->getNext();
+		cout << "\n (╯°□°)╯︵◓" << endl;
+        cout << "\n " << active->getValue().get_name() << " is attacking " << inactive->getValue().get_name() << endl;
         int damage = 1;
 
-		damage *= active->get_attack() / (inactive->get_defense());
+		damage *= active->getValue().get_attack() / (inactive->getValue().get_defense());
 
         cout << "\n (。_°)☆ " << endl;
-        cout << "\n " << inactive->get_name() << " takes damage of " << damage << " and the hp goes from " << inactive->get_health() << " to ";
-        inactive->change_health(inactive->get_health() - damage);
-        cout << inactive->get_health() << endl;
-        if (active == &d) {
-            active = &f;
-            inactive = &d;
-        } else {
-            active = &d;
-            inactive = &f;
-
-        }
+        cout << "\n " << inactive->getValue().get_name() << " takes damage of " << damage << " and the hp goes from " << inactive->getValue().get_health() << " to ";
+        inactive->getValue().change_health(inactive->getValue().get_health() - damage);
+        cout << inactive->getValue().get_health() << endl;
+		
+		active = active->getNext();
 	}
 		
     cout << endl;
     cout << "✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶\n";
     system("figlet VICTORY!!!!! | lolcat");
     cout << "\n ";
-    cout << " 🏆 " << inactive->get_name() << " has defeated " << active->get_name() << " !!!" << endl;
+    cout << " 🏆 " << inactive->getValue().get_name() << " has defeated " << active->getValue().get_name() << " !!!" << endl;
     cout << "\n   (∩ᄑ_ᄑ)⊃*･｡*･:≡( ε:) " << endl;
     cout << endl;
     cout << endl;
     cout << "✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶✶\n";
-
+	
 }
 
 
